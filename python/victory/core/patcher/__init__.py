fa64c977efca6682001dc3a990cb0da5fc73f780
Srizdebnath <srizd449@gmail.com>
2026-04-11 11:10:54 +0530

    feat(engine): initialize code patching module

---
+++ python/victory/core/patcher/__init__.py
@@ -0,0 +1,9 @@
+"""Code Patcher Module - Apply unified diff patches"""
+
+from .patcher import CodePatcher, UnifiedDiffParser, PatchHunk
+
+__all__ = [
+    "CodePatcher",
+    "UnifiedDiffParser", 
+    "PatchHunk",
+]
